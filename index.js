8d994cba13f882149f4695a1fb9244b4dacef0fa
fentyhall <fentymhall@gmail.com>
2021-01-30 17:07:04 -0800

    Done.

--- index.js
+++ index.js
@@ -21,6 +21,11 @@ function greet(string) {
   } else {
     return "Good Evening"
   }
+
+  // const hour = parseInt(timeStr, 10);
+  // if ( hour < 12) return "Good Morning"
+  // if ( hour > 17) return "Good Evening"
+  // return "Good Afternoon"
 }
 
 /* Write your implementation of displayMessage() */
